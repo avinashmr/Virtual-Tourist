c939f59dd7a824ec2506b12b926892649e8cff80
avinashmr <avinash@Avinashs-MacBook-Pro.local>
2016-04-27 10:28:43 -0700

    Initial Commit

---
+++ Virtual Tourist/VTTravelLocationsMapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  VTTravelLocationsMapViewController.swift
+//  Virtual Tourist
+//
+//  Created by Avinash Mudivedu on 4/27/16.
+//  Copyright © 2016 Avinash Mudivedu. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class VTTravelLocationsMapViewController : UIViewController, MKMapViewDelegate {
+
+
+    
+}
