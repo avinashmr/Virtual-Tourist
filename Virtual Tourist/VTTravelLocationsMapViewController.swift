32062af31058f9c45f1be7cce6be53b96930e083
avinashmr <avinash@me.com>
2016-04-28 09:40:24 -0700

    Map View Controller saves locations properly.

--- Virtual Tourist/VTTravelLocationsMapViewController.swift
+++ Virtual Tourist/VTTravelLocationsMapViewController.swift
@@ -9,8 +9,56 @@
 import UIKit
 import MapKit
 
-class VTTravelLocationsMapViewController : UIViewController, MKMapViewDelegate {
+class VTTravelLocationsMapViewController : UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
 
-    
+        mapView.delegate = self
+        restoreMapRegion()
+    }
+
+    @IBAction func addAPin(sender: AnyObject) {
+
+    }
+
+
+    // MARK: - Saving the Map region to restore state
+
+    private func saveMapRegion() {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(mapView.region.center.latitude, forKey: "centerLatitude")
+        defaults.setObject(mapView.region.center.longitude, forKey: "centerLongitude")
+        defaults.setObject(mapView.region.span.latitudeDelta, forKey: "latitudeDelta")
+        defaults.setObject(mapView.region.span.longitudeDelta, forKey: "longitudeDelta")
+    }
+
+    private func restoreMapRegion() {
+        let defaults = NSUserDefaults.standardUserDefaults()
+
+        if let centerLatitude = defaults.objectForKey("centerLatitude") as? Double,
+            let centerLongitude = defaults.objectForKey("centerLongitude") as? Double?,
+            let latitudeDelta = defaults.objectForKey("latitudeDelta") as? Double?,
+            let longitudeDelta = defaults.objectForKey("longitudeDelta") as? Double? {
+
+            mapView.region = MKCoordinateRegionMake(
+                CLLocationCoordinate2D(latitude: centerLatitude, longitude: centerLongitude!),
+                MKCoordinateSpan(latitudeDelta: latitudeDelta!, longitudeDelta: longitudeDelta!)
+            )
+        }
+    }
 }
+
+    // MARK: - MKMapViewDelegate Functions
+
+    extension VTTravelLocationsMapViewController: MKMapViewDelegate {
+
+        func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+            saveMapRegion()
+        }
+//
+//        func mapView(
+    }
